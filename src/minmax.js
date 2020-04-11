cb1abf35a11d47c14fc8a1d13b23f0fd4e3ea0dc
Ahmad.Fz <ahmad.f.zadeh@icloud.com>
2020-04-11 19:16:42 +0430

    update min and max function

--- src/minmax.js
+++ src/minmax.js
@@ -8,14 +8,14 @@ export default function minmax(node, depth, type = 'max') {
     if (type == 'max') {
         value = -Infinity;
         node.getSuccessors().forEach(child => {
-            value = max(value, minmax(child, depth - 1, 'min'));
+            value = Math.max(value, minmax(child, depth - 1, 'min'));
         });
     }
 
     if (type == 'min') {
         value = Infinity;
         node.getSuccessors().forEach(child => {
-            value = min(value, minmax(child, depth - 1, 'max'));
+            value = Math.min(value, minmax(child, depth - 1, 'max'));
         });
     }
 
